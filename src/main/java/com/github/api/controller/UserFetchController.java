db0d4fa1c585602376427023a7f4c6cb1bae1cd1
vkku <kumar.vivekten@gmail.com>
2019-09-28 23:35:33 +0530

    Added POJO, Pretty Printed

--- src/main/java/com/github/api/controller/UserFetchController.java
+++ src/main/java/com/github/api/controller/UserFetchController.java
@@ -1,5 +1,7 @@
 package com.github.api.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.api.model.GithubInfoDTO;
 import com.github.api.service.UserFetchService;
 
 @Controller
@@ -14,12 +19,18 @@ public class UserFetchController {
 
 	@Autowired
 	UserFetchService fetchService;
+	ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
 	@GetMapping("/user/{username}")
-	public ResponseEntity<?> fetchUserDetails(@PathVariable String username) {
+	public ResponseEntity<?> fetchUserDetails(@PathVariable String username) throws IOException {
 
 		String responseJSON = fetchService.getUserDetails(username);
-		ResponseEntity<String> responseEntity = ResponseEntity.status(HttpStatus.OK).body(responseJSON);
+
+		// Pretty Print Logic
+		GithubInfoDTO prettyPrintMessage = mapper.readValue(responseJSON, GithubInfoDTO.class);
+		String prettyResponse = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(prettyPrintMessage);
+
+		ResponseEntity<String> responseEntity = ResponseEntity.status(HttpStatus.OK).body(prettyResponse);
 		return responseEntity;
 
 	}
